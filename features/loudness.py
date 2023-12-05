a6324eef1f0cb53db7172e4d868970fa0e313c92
Julius Gorton <jgorton@arizona.edu>
2023-12-05 14:22:27 -0700

    fixed corrupted wav files

--- features/loudness.py
+++
@@ -1,9 +0,0 @@
-# Loudness
-# Author: Julius Gorton 
-import numpy as np
-
-def rms(audio, start,end):
-    np.sqrt(np.mean(audio[start:end] ** 2))
-
-# TODO - there are other algorithms relating to perceived loudness
-# also, loudness at given frequencies?
--- model/knn.py
+++ model/knn.py
@@ -1,3 +1,4 @@
+import numpy as np
 def kNN(D, labels, K, x):
     """
     D is the list of training vectors
@@ -5,19 +6,33 @@ def kNN(D, labels, K, x):
     K is the number of nearest neighbors
     x is the input whose label we want to find
     """
+    #print(D.shape)
+    #print(labels.shape)
+    #print(x.shape)
+    #assert(False)
     # this gives euclidean distance but hopefully faster
     S = np.linalg.norm(D - x, axis=1)
-    #for sample in D:
-    #    S.append(distance(x, sample))
-    #S_indices = S.argsort()
     S_indices = np.argpartition(S, range(K))
     # we only need to sort the first K  vectors
     #print(S_indices[:K])
     S = S[S_indices[:K]]
     #print(S)
     labels = labels[S_indices[:K]]
-    #print(labels)
-    predicted_label = 0
-    for k in range(K):
-        predicted_label += labels[k]
-    return predicted_label > K /2
+    # plurality vote
+    predicted_label = np.argmax(np.bincount(labels))
+    return predicted_label
+def testErrorKNN(tr_values, tr_labels, K, num_tr_values, test_values, test_labels):
+    """
+    This tests error for the knn algorithm
+    (tr/test)_values are the feature vectors, labels are the corresponding labels
+    K is number of nearest neighbors
+    num_tr_values is number of values from the training set that should be used to train
+    Everything but K (int) is a numpy array
+    
+    """
+    tr_values = tr_values[:num_tr_values]
+    tr_labels = tr_labels[:num_tr_values]
+    # since true == 1, this works for binary classification
+    error = sum([kNN(tr_values, tr_labels, K, test_values[i]) != test_labels[i] \
+                 for i in range(test_values.shape[0])])
+    return error / test_values.shape[0]
